bb797a7ba915c57901bb5aabb0ad058d6da41c53
Pierre TROLLE <pierre.trolle@manomano.com>
2019-07-20 12:03:51 +0200

    fix(build): no unused import

--- front-react/src/bricks/project-table/ProjectTable.jsx
+++ front-react/src/bricks/project-table/ProjectTable.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ProjectHeader from './project-header/ProjectHeader';
 import { default as ProjectBody} from './project-body';
+// eslint-disable-next-line no-unused-vars
 import styles from './ProjectTable.css'
 
 const ProjectTable = () => (
