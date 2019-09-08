63e66653a4adc5d1b83732af411ff6592e9c144f
Pierre TROLLE <pierre.trolle@manomano.com>
2019-09-08 21:21:02 +0200

    fix(test): make pipeline green

--- front-react/src/bricks/project-table/project-body/ProjectBodyModule.jsx
+++ front-react/src/bricks/project-table/project-body/ProjectBodyModule.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import TaskRaw from './task-raw/TaskRaw';
 import PropTypes from 'prop-types';
 
-const MyModule = ({
+const ProjectBodyModule = ({
     tasks,
     fetchTasks,
   }) => {
@@ -10,11 +10,11 @@ const MyModule = ({
 
   useEffect(() => {
     fetchTasks()
-  }, [])
+  }, [fetchTasks])
 
   return (
   <tbody className="project-body">
-    {(isLoading) ? (<tr><td>loading</td></tr>) : null}
+    {isLoading ? (<tr><td>loading</td></tr>) : null}
     {error ? <tr><td>An error has occured</td></tr> : null}
     {(!data) ? (<tr><td>oops not possible?</td></tr>) :
       data.map(task => (<TaskRaw task={task} key={task.id}/>))}
@@ -22,28 +22,32 @@ const MyModule = ({
     );
  };
 
-MyModule.displayName = 'MyModule';
+ProjectBodyModule.displayName = 'ProjectBodyModule';
 
-MyModule.propTypes = {
-  isLoading: PropTypes.bool,
-  error: PropTypes.object,
-  data: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number,
-      name: PropTypes.string,
-      description: PropTypes.string,
-      points: PropTypes.number,
-      createdDate: PropTypes.string,
-      endDate: PropTypes.string,
-    })
-  ),
+ProjectBodyModule.propTypes = {
+  tasks: PropTypes.exact({
+    isLoading: PropTypes.bool,
+    error: PropTypes.object,
+    data: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number,
+        name: PropTypes.string,
+        description: PropTypes.string,
+        points: PropTypes.number,
+        createdDate: PropTypes.string,
+        endDate: PropTypes.string,
+      })
+    ),
+  }),
   fetchTasks: PropTypes.func.isRequired,
 };
 
-MyModule.defaultProps = {
+ProjectBodyModule.defaultProps = {
+  tasks: {
   isLoading: true,
   error: undefined,
   data: []
+  }
 };
 
-export default MyModule;
+export default ProjectBodyModule;
