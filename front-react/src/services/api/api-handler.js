ed12c8b89ba85a89eed14e47bacd526546930487
Pierre TROLLE <pierre.trolle@manomano.com>
2019-09-10 09:03:59 +0200

    chore(refacto): api service

---
+++ front-react/src/services/api/api-handler.js
@@ -0,0 +1,32 @@
+import { logger, token } from '../index';
+import axios from 'axios';
+
+const getApiUriToCall = path => {
+  return `http://localhost:3001/${path}`;
+  // return `${env('API_URL')}${path}`
+};
+
+const getAxiosConfiguration = ({ method, apiPath, data }) => ({
+  url: getApiUriToCall(apiPath),
+  method,
+  data,
+  headers: {
+    ...token.getAccessToken() ? { Authorization: `Bearer ${(token.getAccessToken())}` } : {},
+  },
+});
+
+const handleErrors = error => {
+  logger.error(error);
+  throw error;
+};
+
+const axiosHandler = (method, update) => (apiPath, data) =>
+  Promise
+    .resolve(getAxiosConfiguration({ method, apiPath, data }))
+    .then(axios)
+    .then(update)
+    .catch(handleErrors);
+
+export {
+  axiosHandler
+}
