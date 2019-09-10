bf76cb01654d976e1cbb31b24140e3d43f5cf84f
Pierre TROLLE <pierre.trolle@manomano.com>
2019-09-10 12:45:09 +0200

    chore(move): api service

--- front-react/src/services/api/__test__/api-service.test.js
+++ front-react/src/services/api/__test__/api-service.test.js
@@ -1,4 +1,4 @@
-import apiService from './api-service';
+import apiService from '../api-service';
 
 jest.mock('./api-handler', () => ({ axiosHandler: (method, update) => () => ({ method, update }) }));
 
