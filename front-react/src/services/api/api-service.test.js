f851fb4ba1e458b1c0505937a0fb48f58cf61bdc
Pierre TROLLE <pierre.trolle@manomano.com>
2019-09-10 09:59:15 +0200

    chore(test): api service tested

--- front-react/src/services/api/api-service.test.js
+++ front-react/src/services/api/api-service.test.js
@@ -1,229 +1,51 @@
-import axios from 'axios';
 import apiService from './api-service';
-import env from '../../env/env';
-import { token, logger } from '../index';
 
-jest.mock('../../env/env');
+jest.mock('./api-handler', () => ({ axiosHandler: (method, update) => () => ({ method, update }) }));
 
 describe('apiService', () => {
-  const API_URL = 'http://localhost:3001/';
-
-  beforeEach(() => {
-    logger.error = jest.fn();
-    token.getAccessToken = jest.fn();
-    token.getAccessToken.mockReturnValue('token');
-  });
+  const givenPropsForRamda = { data: 'data', status: 'status' };
 
   describe('get', () => {
-    const path = 'status';
+    it('should call axios handler with correct method and update data', () => {
+      // When
+      const returnedValue = apiService.get();
 
-    describe('when the promise resolves data', () => {
-      const data = { hello: 'world' };
-
-      beforeEach(() => {
-        env.mockReturnValue(API_URL);
-        axios.get = jest.fn();
-        axios.get.mockResolvedValue({ data });
-      });
-
-      it('should call the correct API status endpoint', async () => {
-        // Given
-        const url = `${API_URL}${path}`;
-
-        // When
-        await apiService.get(path);
-
-        // Then
-        expect(axios.get).toHaveBeenCalledWith(url, { "headers": { "Authorization": "Bearer token", "json": true } });
-      });
-
-      it('should return response data', async () => {
-        // Given
-        expect.assertions(1);
-
-        // When
-        const response = await apiService.get(path);
-
-        // Then
-        expect(response).toEqual(data);
-      });
-    });
-
-    describe('when the promise rejects', () => {
-      beforeEach(() => {
-        axios.get = jest.fn();
-        axios.get.mockRejectedValue('error');
-      });
-
-      it('should log the error', async () => {
-        // When
-        try {
-          await apiService.get(path);
-        } catch (error) {
-          // Then
-          expect(logger.error).toHaveBeenCalledWith('error');
-          expect(error).toEqual('error');
-        }
-      });
+      // Then
+      expect(returnedValue).toEqual(expect.objectContaining({ method: 'get' }));
+      expect(returnedValue.update(givenPropsForRamda)).toEqual('data');
     });
   });
 
   describe('post', () => {
-    const path = 'status';
+    it('should call axios handler with correct method and update data', () => {
+      // When
+      const returnedValue = apiService.post();
 
-    describe('when the promise resolves data', () => {
-      const data = { hello: 'world' };
-
-      beforeEach(() => {
-        env.mockReturnValue(API_URL);
-        axios.post.mockResolvedValue({ data });
-      });
-
-      it('should call the correct API status endpoint', async () => {
-        // Given
-        expect.assertions(3);
-        const url = `${API_URL}${path}`;
-
-        // When
-        await apiService.post(path);
-
-        // Then
-        expect(axios.post).toHaveBeenCalledWith(url, { json: true });
-      });
-
-      it('should return response data', async () => {
-        // Given
-        expect.assertions(1);
-
-        // When
-        const response = await apiService.post(path);
-
-        // Then
-        expect(response).toEqual(data);
-      });
-    });
-
-    describe('when the promise rejects', () => {
-      beforeEach(() => {
-        axios.post.mockRejectedValue(new Error('Async error'));
-      });
-
-      it('should log the error', async () => {
-        // Given
-        expect.assertions(3);
-
-        // When
-        await apiService.post(path);
-
-        // Then
-        expect(logger.error).toHaveBeenCalledWith('Async error');
-      });
+      // Then
+      expect(returnedValue).toEqual(expect.objectContaining({ method: 'post' }));
+      expect(returnedValue.update(givenPropsForRamda)).toEqual('data');
     });
   });
 
   describe('put', () => {
-    const path = 'status';
+    it('should call axios handler with correct method and update status', () => {
+      // When
+      const returnedValue = apiService.put();
 
-    describe('when the promise resolves data', () => {
-      const data = { hello: 'world' };
-
-      beforeEach(() => {
-        env.mockReturnValue(API_URL);
-        axios.patch.mockResolvedValue({ data });
-      });
-
-      it('should call the correct API status endpoint', async () => {
-        // Given
-        expect.assertions(3);
-        const url = `${API_URL}${path}`;
-
-        // When
-        await apiService.put(path);
-
-        // Then
-        expect(axios.patch).toHaveBeenCalledWith(url, { json: true });
-      });
-
-      it('should return response data', async () => {
-        // Given
-        expect.assertions(1);
-
-        // When
-        const response = await apiService.put(path);
-
-        // Then
-        expect(response).toEqual(data);
-      });
-    });
-
-    describe('when the promise rejects', () => {
-      beforeEach(() => {
-        axios.patch.mockRejectedValue(new Error('Async error'));
-      });
-
-      it('should log the error', async () => {
-        // Given
-        expect.assertions(3);
-
-        // When
-        await apiService.put(path);
-
-        // Then
-        expect(logger.error).toHaveBeenCalledWith('Async error');
-      });
+      // Then
+      expect(returnedValue).toEqual(expect.objectContaining({ method: 'put' }));
+      expect(returnedValue.update(givenPropsForRamda)).toEqual('status');
     });
   });
 
   describe('delete', () => {
-    const path = 'status';
+    it('should call axios handler with correct method and update status', () => {
+      // When
+      const returnedValue = apiService.delete();
 
-    describe('when the promise resolves data', () => {
-      const data = { hello: 'world' };
-
-      beforeEach(() => {
-        env.mockReturnValue(API_URL);
-        axios.delete.mockResolvedValue({ data });
-      });
-
-      it('should call the correct API status endpoint', async () => {
-        // Given
-        expect.assertions(3);
-        const url = `${API_URL}${path}`;
-
-        // When
-        await apiService.delete(path);
-
-        // Then
-        expect(axios.delete).toHaveBeenCalledWith(url);
-      });
-
-      it('should return response data', async () => {
-        // Given
-        expect.assertions(1);
-
-        // When
-        const response = await apiService.delete(path);
-
-        // Then
-        expect(response).toEqual(data);
-      });
-    });
-
-    describe('when the promise rejects', () => {
-      beforeEach(() => {
-        axios.delete.mockRejectedValue(new Error('Async error'));
-      });
-
-      it('should log the error', async () => {
-        // Given
-        expect.assertions(3);
-
-        // When
-        await apiService.delete(path);
-
-        // Then
-        expect(logger.error).toHaveBeenCalledWith('Async error');
-      });
+      // Then
+      expect(returnedValue).toEqual(expect.objectContaining({ method: 'delete' }));
+      expect(returnedValue.update(givenPropsForRamda)).toEqual('status');
     });
   });
 });
