394f245da83e1ffbdfc691557129ca6380be7a32
Pierre TROLLE <pierre.trolle@manomano.com>
2019-09-11 23:20:02 +0200

    chore(test): move

--- front-react/src/bricks/project-form/__test__/ProjectForm.test.jsx
+++ front-react/src/bricks/project-form/__test__/ProjectForm.test.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { shallow } from 'enzyme'
-import ProjectTable from './ProjectTable';
+import ProjectForm from '../ProjectForm';
 
-describe('ProjectTable', () => {
+describe('ProjectForm', () => {
   describe('template', () => {
     it('should match snapshot', () => {
       // When
-      const wrapper = shallow(<ProjectTable/>)
+      const wrapper = shallow(<ProjectForm/>)
 
       // Then
       expect(wrapper).toMatchSnapshot()
