c3f74d227b53b505a80cc00fe05a3e2a26b9062d
Pierre TROLLE <pierre.trolle@manomano.com>
2019-09-17 19:39:36 +0200

    chore(clean-archi): use more index and clean imports

--- back/src/use_cases/index.js
+++ back/src/use_cases/index.js
@@ -1,7 +1,13 @@
 const { connect } = require('./connect')
 const { refreshAccessToken } = require('./refresh-access-token')
+const { addTask } = require('./add-task')
+const taskService = require('./task-service')
+const meetingService = require('./meeting-service')
 
 module.exports = {
   connect,
   refreshAccessToken,
+  addTask,
+  meetingService,
+  taskService,
 }
