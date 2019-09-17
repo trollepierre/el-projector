09faf7a52a752be7f3379ac24fcdc7e8ca3300ae
Pierre TROLLE <pierre.trolle@manomano.com>
2019-09-17 20:07:03 +0200

    chore(refacto): use env

--- back/src/use_cases/connect.js
+++ back/src/use_cases/connect.js
@@ -1,17 +1,12 @@
 /* eslint-disable no-console */
 const { generateAccessToken, generateRefreshToken, saveTokens } = require('./services/token-service')
-const env = require( '../infrastructure/env/env')
+const { env } = require('../infrastructure/env')
 
 const connect = ({ user }) => {
   // do the database authentication here, with user name and password combination.
-  console.log('process.env.PASSWORD:')
-  console.log(process.env.PASSWORD)
-  console.log('fin process.env.PASSWORD')
-  console.log('env(\'PASSWORD\'):')
-  console.log(env('PASSWORD'))
-  console.log('fin env(\'PASSWORD\')')
-
-  if(user.name !== process.env.PASSWORD) throw new Error('Unauthorized')
+  if (user.name !== env('PASSWORD')) {
+    throw new Error('Unauthorized')
+  }
 
   const token = generateAccessToken(user)
   const refreshToken = generateRefreshToken(user)
