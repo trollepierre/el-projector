24eef542af4278dfad97ada7b5d77acebc3784dc
Pierre TROLLE <pierre.trolle@manomano.com>
2019-09-19 11:15:49 +0200

    fix(auth): last reviews are taken

--- front-react/src/components/Input/Input.jsx
+++ front-react/src/components/Input/Input.jsx
@@ -1,9 +1,3 @@
-/**
- * Copyright (c) Colibri SAS - ManoMano
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import cn from 'classnames';
--- front-react/src/components/Input/Input.test.jsx
+++ front-react/src/components/Input/Input.test.jsx
@@ -1,15 +1,9 @@
-/**
- * Copyright (c) Colibri SAS - ManoMano
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
-import React from 'react';
-import { shallow } from 'enzyme';
-import Input from '../Input';
+import React from 'react'
+import { shallow } from 'enzyme'
+import Input from '.'
 
 describe('Input', () => {
-  let wrapper;
+  let wrapper
 
   beforeEach(() => {
     wrapper = shallow(
@@ -21,45 +15,39 @@ describe('Input', () => {
         helper="helper"
         value="value"
       />
-    );
-  });
+    )
+  })
 
   it('should match snapshot', () => {
-    expect(wrapper).toMatchSnapshot();
-  });
+    expect(wrapper).toMatchSnapshot()
+  })
 
   it('should render a input', () => {
-    expect(wrapper.find('input').length).toEqual(1);
-  });
+    expect(wrapper.find('input')).toHaveLength(1)
+  })
 
   it('should render a label', () => {
-    expect(wrapper.find('label').text()).toEqual('label');
-  });
+    expect(wrapper.find('label').text()).toEqual('label')
+  })
 
   it('should render an helper span', () => {
     wrapper = shallow(
       <Input type="text" name="name" value="value" helper="helper" />
-    );
-    expect(wrapper.find('span').text()).toEqual('helper');
-  });
+    )
+    expect(wrapper.find('span').text()).toEqual('helper')
+  })
 
   it('should render an error span', () => {
     wrapper = shallow(
       <Input type="text" name="name" value="value" error="error" />
-    );
-    expect(wrapper.find('span').text()).toEqual('error');
-  });
+    )
+    expect(wrapper.find('span').text()).toEqual('error')
+  })
 
   it('should render an indicator', () => {
     wrapper = shallow(
       <Input type="text" name="name" value="value" indicator="indicator" />
-    );
-    expect(wrapper.find('span').text()).toEqual('indicator');
-  });
-
-
-
-
-
-
-});
+    )
+    expect(wrapper.find('span').text()).toEqual('indicator')
+  })
+})
