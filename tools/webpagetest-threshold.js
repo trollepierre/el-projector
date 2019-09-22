020a28fccf66b33b789c7f0585b334309c3644a9
Pierre TROLLE <pierre.trolle@manomano.com>
2019-09-22 15:55:04 +0200

    chore(final-tests): update threshold

--- tools/webpagetest-threshold.js
+++ tools/webpagetest-threshold.js
@@ -2,8 +2,8 @@ module.exports = {
   'median': {
     'firstView': {
       'requests': 10, // first value : 20 ; results = 6
-      'render': 6500, // first value : 2000 ; results = 6400
-      'loadTime': 6500, // first value : 3000 ; results = 6363
+      'render': 1500, // first value : 2000 ; results = 1400
+      'loadTime': 1500, // first value : 3000 ; results = 1322
       'score_gzip': {
         'min': 30, // first value : 30 ; results = 32
       },
